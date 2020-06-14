21851985737413890170c2712ab272b7598f9dc3
khwabscript <59730462+khwabscript@users.noreply.github.com>
2020-06-14 13:58:29 +0300

    Delete query.sql

--- query.sql
+++
@@ -1,1 +0,0 @@
-SELECT products.name, categories.name FROM products LEFT JOIN category_product ON products.id = category_product.product_id LEFT JOIN categories ON categories.id = category_product.category_id
